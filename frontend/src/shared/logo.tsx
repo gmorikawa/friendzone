c31a88f8d95b7311be3a8108d2054d67d7194b5d
gmorikawa <morikawa.gabriel@outlook.jp>
2026-01-24 12:05:55 +0900

    frontend: Refactoring logo font family

--- frontend/src/shared/logo.tsx
+++ frontend/src/shared/logo.tsx
@@ -1,9 +1,19 @@
-import { Paragraph } from "@/components/typography/paragraph";
+import { Title } from "@/components/typography/title";
+import { Environment } from "@/config/environment";
 
 export function Logo() {
     return (
-        <Paragraph textAlign="center">
-            Friendzone
-        </Paragraph>
+        <Title
+            level={4}
+            textAlign="center"
+            sx={{
+                fontFamily: "'Walter Turncoat', cursive",
+                fontWeight: 400,
+                fontStyle: "normal",
+                marginBottom: 2,
+            }}
+        >
+            {Environment.APPLICATION_NAME}
+        </Title>
     );
 }
