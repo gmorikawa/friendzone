b57bd3ea8b5fef02b6ca3cf5127842ebc6ac5910
gmorikawa <morikawa.gabriel@outlook.jp>
2026-01-24 12:14:26 +0900

    frontend: Added size property to style font size in Paragraph component

--- frontend/src/components/typography/paragraph.tsx
+++ frontend/src/components/typography/paragraph.tsx
@@ -1,12 +1,24 @@
 import { Typography, type TypographyProps } from "@mui/material";
 
 export interface ParagraphProps extends Omit<TypographyProps, "variant"> {
-    align?: "left" | "center" | "right" | "justify";
+    size?: "small" | "medium" | "large";
 }
 
-export function Paragraph({ children, align, ...props }: ParagraphProps) {
+export function Paragraph({ children, size = "medium", ...props }: ParagraphProps) {
+    const generateVariant = () => {
+        switch (size) {
+            case "small":
+                return "caption";
+            case "large":
+                return "body1";
+            case "medium":
+            default:
+                return "body2";
+        }
+    };
+
     return (
-        <Typography {...props} variant="body1">
+        <Typography {...props} variant={generateVariant()}>
             {children}
         </Typography>
     );
