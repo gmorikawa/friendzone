94236e0af8ce3500bab403213d77cc74efed093b
gmorikawa <morikawa.gabriel@outlook.jp>
2026-01-27 18:37:20 +0900

    frontend (config): Vite configuration to allow a specific host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const PORT = 3000;
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
@@ -9,13 +11,16 @@ export default defineConfig({
         tsconfigPaths(),
     ],
     preview: {
-        port: 5173,
+        port: PORT,
         strictPort: true,
     },
     server: {
-        port: 5173,
+        port: PORT,
         strictPort: true,
         host: true,
-        origin: "http://0.0.0.0:5173",
+        origin: `http://0.0.0.0:${PORT}`,
+        allowedHosts: [
+            "friendzone.gmorikawa.dev"
+        ],
     },
 });
