f2265ce2ad0831aa8a0a86bd2213a52b7d3d679c
gmorikawa <morikawa.gabriel@outlook.jp>
2026-01-27 13:25:20 +0900

    ops: Configuring backend and frontend applications on Docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,4 +8,14 @@ export default defineConfig({
         react(),
         tsconfigPaths(),
     ],
+    preview: {
+        port: 5173,
+        strictPort: true,
+    },
+    server: {
+        port: 5173,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:5173",
+    },
 });
