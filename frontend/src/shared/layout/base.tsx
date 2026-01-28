2278e1e5f500feb5a8dc9f62e5337037d62edaf5
gmorikawa <morikawa.gabriel@outlook.jp>
2026-01-28 18:06:34 +0900

    frontend (fix): Footer menu on mobile view hiding fixed with dynamic viewport height

--- frontend/src/shared/layout/base.tsx
+++ frontend/src/shared/layout/base.tsx
@@ -6,7 +6,7 @@ export function BaseLayout() {
         <Container
             sx={{
                 width: "100%",
-                height: "100vh"
+                height: "100dvh",
             }}
         >
             <Outlet />
